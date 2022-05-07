1035c54171f9ddc70a9928acac90159644c00468
akdlsz21 <akdlsz21@gmail.com>
2022-05-07 15:31:31 +0900

    binary tree recursive generation done

--- hw509/509_1.c
+++ hw509/509_1.c
@@ -10,21 +10,95 @@ typedef struct _node{
 
 int randomInt(int start, int end);
 void generateChildrenOfRoot(Node* root);
+void appendChild(Node* root, int nodeVals[], int directionArr[], int* j);
+int* DFS(Node* node, int resultOfDfs[]);
+
+
+
+
+
 
 int main(){
    srand(time(NULL));
-   // Making the tree;
+   // nodeVal array;
+   int nodeN = randomInt(10, 20);
+   int nodeVals[nodeN];
+   int directionArr[nodeN];
+   
+   for(int i = 0; i < nodeN; i++){
+      nodeVals[i] = randomInt(1, 100);
+      directionArr[i] = randomInt(1, 2);
+
+   }
+
 
    
    // init the root;
    Node* rootNode = malloc(sizeof(Node));
-   rootNode->val = randomInt(1, 100);
+   // rootNode->val = randomInt(1, 100);
+   rootNode->val = 500;
 
    // make children of rootNode(10 ~ 20)
-   for(int i = 0; i < 10; i++){
-      
+   int j = 0;
+   for(int i = 0; i < nodeN; i++){
+      appendChild(rootNode, nodeVals, directionArr, &j);
    }
+   
+   int resultOfDfs[21];
+   DFS(rootNode, resultOfDfs);
+   printf("\n\nRecursive Node tree(pre)\n==========================================================\n");
+   for(int i = 0; i < nodeN; i++){
+      printf("%d - ", resultOfDfs[i]);
+   }
+   printf("\n==========================================================\n");
 
+
+}
+
+int* DFS(Node* node, int resultOfDfs[]){
+   
+   int i = 0;
+
+   void helper(Node* node){
+
+      if(node == NULL) return;
+
+      resultOfDfs[i] = node->val;
+      i++;
+      if(node->left) helper(node->left);
+      if(node->right) helper(node->right);
+
+
+   }
+   helper(node);
+
+}
+
+
+
+void appendChild(Node* root, int nodeVals[], int directionArr[], int* j){
+   
+   
+
+   if(directionArr[*j]){
+      if(root->left == NULL){
+         Node* newNode = malloc(sizeof(Node));
+         newNode->val = nodeVals[*j];
+         *j = *j + 1;
+         root->left = newNode;
+      }
+      
+      else appendChild(root->left, nodeVals, directionArr, j);
+   }else if(directionArr[*j] ==2){
+      if(root->right == NULL){
+         Node* newNode = malloc(sizeof(Node));
+         newNode->val = nodeVals[*j];
+         *j = *j + 1;
+         root->right = newNode;
+      }
+      
+      else appendChild(root->right, nodeVals, directionArr, j);
+   }
 
 }
 
